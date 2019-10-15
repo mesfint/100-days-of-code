cb3ad3fe664f6eefff8afe97308203b4295247ee
mesfin <mesfint@users.noreply.github.com>
2019-10-15 18:55:44 +0300

    Update server2.js

--- codes/frontEndMaster_Web_Dev/node-example/server2.js
+++ codes/frontEndMaster_Web_Dev/node-example/server2.js
@@ -1,1 +1,38 @@
+const express = require("express");
+const path = require("path");
 
+const insults = [
+  "Code doesn’t compile",
+  "Full of antipatterns",
+  "Cargo cult programmer",
+  "Hackish",
+  "Randomness",
+  "You're programming! How cool is that?",
+  "Code doesn’t compile",
+  "Literally the most pathetic thing I've read, ever",
+  "Bunch of drama alert drama queens imo"
+];
+
+function getRandomInsult() {
+  const randomIndex = Math.floor(Math.random() * insults.length);
+  return insults[randomIndex];
+}
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/insult", function(req, res) {
+  res
+    .json({
+      insult: getRandomInsult()
+    })
+    .end();
+});
+
+app.use("/public", express.static("./public"));
+
+app.listen(3000);
+console.log("listening on http://localhost:3000");
